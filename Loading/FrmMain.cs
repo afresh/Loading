4bdc982fc903f4dd022478ba81dcbc1715fb1c36
danding_ge <danding_ge@126.com>
2018-01-20 23:09:46 +0800

    .

--- Loading/FrmMain.cs
+++ Loading/FrmMain.cs
@@ -12,7 +12,7 @@ namespace Loading
 
         private void BtnLoading_Click(object sender, System.EventArgs e)
         {
-            LoadingHelper.ShowLoading("有朋自远方来，不亦乐乎。", this, (obj) =>
+            LoadingHelper.ShowLoading("有朋自远方来，不亦乐乎。", this, o =>
             {
                 //这里写处理耗时的代码，代码处理完成则自动关闭该窗口
                 Thread.Sleep(10000);
